4739e800c2fa7d11a6bd37197cd2d2f8b26bb873
Erin Dieringer <ediering@andrew.cmu.edu>
2016-04-18 11:50:25 -0400

    has secure password added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   # get modules to help with some functionality
   include CreameryHelpers::Validations
 
@@ -10,9 +12,22 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :message => "is not a valid format"
   validate :employee_is_active_in_system
   
+  ROLES = [['Employee', 'employee', :employee],['Manager', 'manager', :manager],['Administrator', 'admin', :admin]]
+
+  def role?(authorized_role)
+     return false if self.employee.role.nil?
+     self.employee.role.to_sym == authorized_role
+  end
+
+
   private
+  def self.authenticate(email,password)
+      find_by_email(email).try(:authenticate, password)
+  end
+
   def employee_is_active_in_system
     is_active_in_system(:employee)
   end
+
 
 end
