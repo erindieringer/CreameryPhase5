67d25952f4b162957cf928f092d3fc0a095af3c0
Erin Dieringer <ediering@andrew.cmu.edu>
2016-04-27 12:12:10 -0400

    assignment picker works for just managers employees too

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,7 +1,11 @@
 module ShiftsHelper
 	
 	def get_assignment_options
-		Assignment.current.all.to_a.map{ |a| [ "#{a.employee.name}"]}
+		if logged_in? && current_user.employee.role == "manager"
+			Assignment.current.for_store(current_user.employee.current_assignment.store_id).all.to_a.map{ |a| [ "#{a.employee.name}"]}.sort
+		else
+			Assignment.current.all.to_a.map{ |a| [ "#{a.employee.name}"]}
+		end
 	end
 
 end
