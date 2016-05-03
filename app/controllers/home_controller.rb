4c31d8158d24635ef1f1d656a05bbe971b098efc
Erin Dieringer <ediering@andrew.cmu.edu>
2016-05-03 17:59:28 -0400

    some views fixed up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,19 +2,19 @@ class HomeController < ApplicationController
 
   def home
   	if logged_in? && current_user.employee.role == "manager"
-      @incomplete_shifts = Shift.chronological.for_store(current_user.employee.current_assignment.store_id).past.incomplete.paginate(page: params[:page]).per_page(5)
-    	@todays_shifts = Shift.for_store(current_user.employee.current_assignment.store_id).for_next_days(0).paginate(page: params[:page]).per_page(5)
+      @incomplete_shifts = Shift.chronological.for_store(current_user.employee.current_assignment.store_id).past.incomplete.paginate(page: params[:page1]).per_page(5)
+    	@todays_shifts = Shift.for_store(current_user.employee.current_assignment.store_id).for_next_days(0).paginate(page: params[:page2]).per_page(5)
       @current_store_flavors = current_user.employee.current_assignment.store.store_flavors
       @shift = @incomplete_shifts.first
     else
       @incomplete_shifts = Shift.incomplete.all
     end
-    @active_flavors = Flavor.active.alphabetical.paginate(page: params[:page]).per_page(10)
+    @active_flavors = Flavor.active.alphabetical.paginate(page: params[:page3]).per_page(10)
     @active_stores = Store.active.alphabetical
-    @shifts_today = Shift.for_next_days(0).by_store.paginate(page: params[:page]).per_page(5)
-    @active_employees = Employee.active.regulars.all.paginate(page: params[:page]).per_page(5)
-    @active_managers = Employee.active.managers.all.paginate(page: params[:page]).per_page(5)
-    @active_admins = Employee.active.admins.all.paginate(page: params[:page]).per_page(5)
+    @shifts_today = Shift.for_next_days(0).by_store.paginate(page: params[:page4]).per_page(5)
+    @active_employees = Employee.active.regulars.all.paginate(page: params[:page5]).per_page(5)
+    @active_managers = Employee.active.managers.all.paginate(page: params[:page6]).per_page(5)
+    @active_admins = Employee.active.admins.all.paginate(page: params[:page7]).per_page(5)
 
 
 
